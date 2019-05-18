1014d55404e6239e2603348336bdf85c8f59f2bc
Mohammad-Quanit <muhammadquanit@gmail.com>
2019-05-19 03:46:56 +0500

    Login UI Ready

--- MEAN-App/Frontend-code/src/app/login/login.component.ts
+++ MEAN-App/Frontend-code/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {}
+  constructor(private _auth: RegisterService) { }
 
   ngOnInit() {
   }
